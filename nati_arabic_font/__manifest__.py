88f9c576a46b3b608b44cb87ee54efdddc10f464
mali <mahlhel@gmail.com>
2020-11-16 05:09:01 +0300

    [UPT] update module version

--- nati_arabic_font/__manifest__.py
+++ nati_arabic_font/__manifest__.py
@@ -1,22 +1,24 @@
- # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 {
     'name': "Nati arabic font",
 
     'summary': """
-        change defult font to nice arabic font""",
+         change defult font to nice arabic font""",
 
     'description': """
-        Change the defult arabic font of the all interfaces with a beautiful one preferred by the Arabic user
- ,
-    """,
-    'author': "Muhlhel ITS",
+         Change the defult arabic font of the all interfaces with a beautiful one preferred by the Arabic user
+  ,
+     """,
+    'author': "Mali, MuhlhelITS",
     'website': "http://natigroup.biz",
     'category': 'Localization',
-    'version': '14.0.0.0',
+    'version': '12.0.0.0',
     'depends': ['web'],
     'qweb': [],
-    'data': ['views/linkestatic.xml',   
-    ],
+    'data': ['views/linkestatic.xml',
+             ],
+    'images': ['static/description/banner.png'],
+    'license': 'AGPL-3',
     'auto_install': True,
     'installable': True,
 }
