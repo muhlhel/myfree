6717c1bec2bfb4a07842f5edc51a4c0b56fb9b3b
mali <mahlhel@gmail.com>
2020-11-17 00:39:31 +0300

    [ADD] add module for account chart of yemen and sudan

---
+++ nati_l10n_ye/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Yemen- Accounting-Muhlhel-standard',
+    'version': '13.0.0.0',
+    'author': "Mali, MuhlhelITS",
+    'website': "http://natigroup.biz",
+    'category': 'Localization',
+    'description': """
+     Arabic localization for most arabic countries and Yemen.
+    """,
+    'depends': ['account', 'l10n_multilang'],
+    'data': [
+        'data/account_chart_template_data.xml',
+        'data/account.group.csv',
+        'data/account.account.template.csv',
+        'data/l10n_ye_chart_data.xml',
+        'data/account_chart_template_configure_data.xml',
+    ],
+     'images': ['static/description/banner.png'],
+     'license': 'AGPL-3',
+     'application': True,
+    'post_init_hook': 'load_translations',
+}
