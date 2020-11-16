f6ac5ceb375b9afc2acb76fa73f831c04feedb35
mali <mahlhel@gmail.com>
2020-11-16 08:10:26 +0300

    [ADD]Add new module, for chart of accounting

--- l10n_ye/__manifest__.py
+++ l10n_ye/__manifest__.py
@@ -2,14 +2,14 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'Saudi Arabia - Accounting-Muhlhel-standard',
+    'name': 'Yemen- Accounting-Muhlhel-standard',
     'version': '13.0.0.0',
     'author': "Mali, MuhlhelITS",
     'website': "http://natigroup.biz",
     'category': 'Localization',
     'description': """
- Arabic localization for most arabic countries and Saudi Arabia.
-""",
+     Arabic localization for most arabic countries and Yemen.
+    """,
     'depends': ['account', 'l10n_multilang'],
     'data': [
         'data/account_chart_template_data.xml',
@@ -18,8 +18,8 @@
         'data/l10n_ye_chart_data.xml',
         'data/account_chart_template_configure_data.xml',
     ],
-    'images': ['static/description/banner.png'],
-    'license': 'AGPL-3',
-    'application': True,
+     'images': ['static/description/banner.png'],
+     'license': 'AGPL-3',
+     'application': True,
     'post_init_hook': 'load_translations',
 }
