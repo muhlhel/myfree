ac0337b202898bebb2ce4fccb640fcec13a2f8e0
mali <mahlhel@gmail.com>
2020-11-16 04:47:04 +0300

    [UPT] module virsion

--- nati_arabic_font/__manifest__.py
+++ nati_arabic_font/__manifest__.py
@@ -9,10 +9,10 @@
         Change the defult arabic font of the all interfaces with a beautiful one preferred by the Arabic user
  ,
     """,
-    'author': "Muhlhel ITS",
+    'author': "Mali, MuhlhelITS",
     'website': "http://natigroup.biz",
     'category': 'Localization',
-    'version': '14.0.0.0',
+    'version': '13.0.0.0',
     'depends': ['web'],
     'qweb': [],
     'data': ['views/linkestatic.xml',   
