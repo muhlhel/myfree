ae0101f72d27a95af2b4135bfca872cecbc73b3d
Muhlhel Ali <muhlhel@gmail.com>
2021-08-19 23:57:05 +0300

    a new update

--- nati_reports_sales/models/sale_order.py
+++ nati_reports_sales/models/sale_order.py
@@ -14,3 +14,6 @@ class SaleOrder(models.Model):
             rec.num_word = str(rec.currency_id.amount_to_text(rec.amount_total))
 
     num_word = fields.Char(string=_("Amount In Words:"), compute='_compute_amount_in_word')
+
+    def action_print(self):
+        return self.env.ref('nati_reports_sales.custom_action_report_SO').report_action(self)
