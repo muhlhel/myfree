2df91f348bb6826ba19a03c4e98cea2b16d1794b
Muhlhel Ali <muhlhel@gmail.com>
2021-09-21 18:59:20 +0300

    update

--- nati_reports_base_style/__manifest__.py
+++ nati_reports_base_style/__manifest__.py
@@ -13,7 +13,7 @@ english and arabic lable,for both LTR and RTL
     'author': "Mali, MuhlhelITS",
     'website': "http://natigroup.biz",
     'category': 'Extra Tools',
-    'license': 'OPL-1',
+    'license': 'LGPL-3',
     'version': '14.0.0.0',
     'depends': ['base','nati_arabic_font'],
     'qweb': [],
