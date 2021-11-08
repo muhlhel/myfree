a473b0501279fa82f76b20b5ed376f6afd608a43
Muhlhel Ali <muhlhel@gmail.com>
2021-11-08 18:29:42 +0300

    upgrade fro 14-15

---
+++ nati_reports_base_style/__manifest__.py
@@ -0,0 +1,31 @@
+ # -*- coding: utf-8 -*-
+{
+    'name': "Nati Reports Base Style",
+
+    'summary': """
+        Redesign of all report, keeping the official and modern look """,
+
+    'description': """
+Many modern designs and shapes, with additional features, 
+for example: line numbers, numbers in words, compatibility with different printing options,
+english and arabic lable,for both LTR and RTL
+    """,
+    'author': "Mali, MuhlhelITS",
+    'website': "http://muhlhel.com",
+    'category': 'Extra Tools',
+    'license': 'LGPL-3',
+    'version': '14.0.0.0',
+    'depends': ['base','nati_arabic_font'],
+    'qweb': [],
+    'data': ['views/main_inherit.xml',
+
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'assets': {
+        'web.report_assets_common': [
+            'nati_reports_base_style/static/src/scss/variables.scss',
+            'nati_reports_base_style/static/src/scss/invoice_style.scss',
+        ],
+    }
+}
